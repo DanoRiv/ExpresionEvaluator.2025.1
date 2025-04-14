e0c1fe72c5699e102c142f677284b2ab837b9afe
DanoRiv <danio260496@gmail.com>
2025-04-13 20:48:30 -0500

    Fix typo

--- Evaluator/Evaluator.UI.Console/Program.cs
+++ Evaluator/Evaluator.UI.Console/Program.cs
@@ -2,12 +2,12 @@
 
 try
 {
-    var resut2 = FunctionEvaluator.Evalute("4*5/(4+6)");
-    var resut1 = FunctionEvaluator.Evalute("4*(5+6-(8/2^3)-7)-1");
+    var result1 = FunctionEvaluator.Evalute("4*5/(4+6)");
+    var result2 = FunctionEvaluator.Evalute("4*(5+6-(8/2^3)-7)-1");
     var result3 = FunctionEvaluator.Evalute("9^(1/2)");
-    Console.WriteLine(resut1);
+    Console.WriteLine(result1);
     Console.WriteLine(result2);
-    Console.WriteLine(resut3);
+    Console.WriteLine(result3);
 }
 catch (Exception ex)
 {
